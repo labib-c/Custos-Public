7fe10c40be9cc393950f89460d626ff6e18b5e56
labib <labibc01@gmail.com>
2020-10-20 13:57:09 -0400

    added basic script to run docker container

---
+++ client/run_docker.sh
@@ -0,0 +1,10 @@
+docker build -t client:dev .
+
+docker run \
+    -it \
+    --rm \
+    -v ${PWD}:/app \
+    -v /app/node_modules \
+    -p 3001:3000 \
+    -e CHOKIDAR_USEPOLLING=true \
+    client:dev
