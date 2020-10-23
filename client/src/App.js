2570da7d2f6f1f7d96198ac1e3a16af6626970b6
labib <labibc01@gmail.com>
2020-10-22 21:04:21 -0400

    added sentry for react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './App.css';
+
+Sentry.init({
+  dsn: "https://5dbbf50c14ee4124ad7a7e5124be414a@o358880.ingest.sentry.io/5450618",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 function App() {
   return (
