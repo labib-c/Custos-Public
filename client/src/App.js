65e20a3715d40e69ad69954146820a04c4195a7e
labib <labibc01@gmail.com>
2020-11-11 19:25:40 -0500

    updated logos and app name

--- client/src/App.js
+++ client/src/App.js
@@ -23,20 +23,6 @@ function App() {
       <Sidebar>
         <DataTable></DataTable>
       </Sidebar>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Custos Web Application
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
