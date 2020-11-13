7a8092396965483d98ee595feb5766fc30c96d30
labib <labibc01@gmail.com>
2020-11-12 20:23:02 -0500

    making tests pass

--- client/src/Components/TableComponent/Table.test.js
+++ client/src/Components/TableComponent/Table.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import DataTable from './DataTable';
+import {rows, columns} from './../../mockData/tableData'
 
 it("renders", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<DataTable></DataTable>, div)
+    ReactDOM.render(<DataTable rows={rows} columns={columns}></DataTable>, div)
 })
