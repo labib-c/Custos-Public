033f0660aafd3ccfa7298954a4c75c2f02d04b37
labib <labibc01@gmail.com>
2020-11-28 20:19:27 -0500

    added place holder for explaination

--- client/src/Pages/EventAlert.js
+++ client/src/Pages/EventAlert.js
@@ -4,12 +4,20 @@ import ShapValueChart from '../Components/ShapValueChart'
 import DataTable from '../Components/DataTable'
 import {shapData} from '../mockData/shapData'
 import {rows, columns} from './../mockData/tableData';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    header: {
+        paddingLeft: "15px"
+    }
+});
 
 export default function EventAlert(props) {
-
+    const classes = useStyles()
     return (
         <Sidebar>
-            <h1 style={{paddingLeft: "15px"}}>Event: {props.match.params.eventId}</h1>
+            <h1 className={classes.header}>Event: {props.match.params.eventId}</h1>
+            <p className={classes.header} >----- Explanation about SHAP Values can go here -----</p>
             <ShapValueChart data={shapData} header={"SHAP Scores"}></ShapValueChart>
             <DataTable rows={rows} columns={columns} header={"Related Events"}></DataTable>
 
