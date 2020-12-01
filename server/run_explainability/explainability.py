7d94615151111d08e1260cc1cad9ce2abb306f86
Conor Vedova <conor.vedova@mail.utoronto.ca>
2020-11-30 23:05:53 -0500

    Created base files

---
+++ server/run_explainability/explainability.py
@@ -0,0 +1,11 @@
+import json
+import pandas as pd
+
+def convert_jsons(example, background):
+    background_df = pd.DataFrame()
+    for example in background.each():
+        example.val()
+        #pd.read_json
+
+def get_custos_score(example, background):
+    convert_jsons(example, background)
