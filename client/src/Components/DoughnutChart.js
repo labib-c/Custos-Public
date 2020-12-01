a5430ed03bd4e6c2db9700af2dca30550d2e2d4d
labib <labibc01@gmail.com>
2020-12-01 01:00:09 -0500

    pass build

--- client/src/Components/DoughnutChart.js
+++ client/src/Components/DoughnutChart.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+// import Button from '@material-ui/core/Button';
+// import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { ResponsivePie } from '@nivo/pie'
 
 const useStyles = makeStyles({
@@ -18,9 +18,9 @@ export default function Doughnut(props) {
     const classes = useStyles()
     const [filter, setFilter] = React.useState("1m")
 
-    const handleToggle = (event) => {
-        setFilter(event.currentTarget.value)
-    }
+    // const handleToggle = (event) => {
+    //     setFilter(event.currentTarget.value)
+    // }
 
     const selectData = (data) => {
         switch(filter) {
