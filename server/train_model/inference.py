0f3ec1ff8f68ecab266832f7636d9db32700f974
Conor Vedova <conor.vedova@mail.utoronto.ca>
2020-12-02 19:12:34 -0500

    Fixed spacing

--- server/train_model/inference.py
+++ server/train_model/inference.py
@@ -17,21 +17,17 @@ def threshold_loss(data_loss, threshold_number):
 def create_confusion(data, model):
     #Find true anomaly values.
     data_in = data.drop(["Redteam"], axis=1)
-    
     num_red = data["Redteam"].sum()
     num_total = len(data.index)
     threshold_percent = num_red / num_total
     print(f"{threshold_percent}% of data is anomalous.")
-    
     if num_red == 0:
         num_red = 1
-    
     data_prediction = model.predict(data_in)
     data_loss = mean_squared_error(data_in.values.tolist(), data_prediction)
     data_loss = pd.Series(data_loss)
     predicted = threshold_loss(data_loss, num_red)
     y_true = data["Redteam"]
-    
     return confusion_matrix(y_true, predicted)
 
 def main(model_file="model/model.h5", data_csv="preproc_data.csv"):
