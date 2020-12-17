fa5acd275de7a58c03df1d35e1d15fd66286fc4c
Julian Nadeau <julian@jnadeau.ca>
2020-12-17 17:35:14 -0500

    Remove tracing

--- client/src/App.js
+++ client/src/App.js
@@ -23,11 +23,7 @@ Sentry.init({
   dsn: "https://5dbbf50c14ee4124ad7a7e5124be414a@o358880.ingest.sentry.io/5450618",
   integrations: [
     new Integrations.BrowserTracing(),
-  ],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
+  ]
 });
 
 function App() {
